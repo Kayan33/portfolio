20a5adc48b65bb5db9b54e0a6abd71fce2b7dde9
Kayan33 <kayanpereira33@gmail.com>
2024-08-30 15:34:40 -0300

    responsividade em: formulario, Soft skills e hard skills

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
-// ..
-AOS.init();
+function disableAOSForSmallScreens() {
+  if (window.innerWidth <= 800) {
+    document.querySelectorAll('[data-aos]').forEach(function(element) {
+      element.removeAttribute('data-aos');
+      element.removeAttribute('data-aos-duration');
+    });
+    AOS.init({ disable: true }); // Desativa AOS para telas pequenas
+  } else {
+    AOS.init(); // Inicializa o AOS normalmente para telas maiores
+  }
+}
 
+// Chama a função ao carregar a página
+disableAOSForSmallScreens();
 
+// Chama a função ao redimensionar a janela
+window.addEventListener('resize', disableAOSForSmallScreens);
