98b42b8474af04b8ed1dbcafd82ea056e49acd13
Kayan33 <kayanpereira33@gmail.com>
2024-10-27 18:42:08 -0300

    teste

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-// function disableAOSForSmallScreens() {
-//   if (window.innerWidth <= 800) {
-//     document.querySelectorAll('[data-aos]').forEach(function(element) {
-//       element.removeAttribute('data-aos');
-//       element.removeAttribute('data-aos-duration');
-//     });
-//     AOS.init({ disable: true }); // Desativa AOS para telas pequenas
-//   } else {
-//     AOS.init(); // Inicializa o AOS normalmente para telas maiores
-//   }
-// }
+function disableAOSForSmallScreens() {
+  if (window.innerWidth <= 800) {
+    document.querySelectorAll('[data-aos]').forEach(function(element) {
+      element.removeAttribute('data-aos');
+      element.removeAttribute('data-aos-duration');
+    });
+    AOS.init({ disable: true }); // Desativa AOS para telas pequenas
+  } else {
+    AOS.init(); // Inicializa o AOS normalmente para telas maiores
+  }
+}
 
-// // Chama a função ao carregar a página
-// disableAOSForSmallScreens();
+// Chama a função ao carregar a página
+disableAOSForSmallScreens();
 
-// // Chama a função ao redimensionar a janela
-// window.addEventListener('resize', disableAOSForSmallScreens);
+// Chama a função ao redimensionar a janela
+window.addEventListener('resize', disableAOSForSmallScreens);
